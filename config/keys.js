18ecdec408d9f670d9666cdfae6ec1cd81f49841
claire froelich <claire.froelich@gmail.com>
2020-06-12 18:52:54 -0400

    add passport

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb+srv://sujiru-admin:su_ji-ru_4649@sujiru-oi093.mongodb.net/Sujiru?retryWrites=true&w=majority"
+  mongoURI: "mongodb+srv://sujiru-admin:su_ji-ru_4649@sujiru-oi093.mongodb.net/Sujiru?retryWrites=true&w=majority",
+  secretOrKey: "secret"
 }
